815fb195070f0c2a4528ee0f70a5bd0d11966568
Kchanez <kanounchanez66@gmail.com>
2025-01-29 17:53:01 +0100

    ajout des pop up

--- Pages/script/chambre.js
+++ Pages/script/chambre.js
@@ -5,7 +5,7 @@ function openImageInNewTab() {
         { src: "/Images/Chambre/Armoire.svg", isBaseImage: false, width: "24%", top: "15%", left: "32%" },
         { src: "/Images/Chambre/Coffre.svg", isBaseImage: false, width: "12%", top: "57%", left: "10%" },
         { src: "/Images/Chambre/Etiquette.svg", isBaseImage: false, width: "17%", top: "3%", left: "5%" },
-        { src: "/Images/Chambre/CadreFamille.svg", isBaseImage: false, width: "6%", top: "21%", left: "8.9%" }
+        { src: "/Images/Chambre/CadreFamille.png", isBaseImage: false, width: "6%", top: "21%", left: "8.9%" }
     ];
     
 
@@ -19,7 +19,7 @@ function openImageInNewTab() {
         body.style.left = "0";
         body.style.width = "100%";
         body.style.height = "100%";
-        //body.style.background = "black"; 
+      //body.style.background = "black"; 
         body.style.display = "flex";
         body.style.justifyContent = "center";
         body.style.alignItems = "center";
@@ -43,7 +43,7 @@ function openImageInNewTab() {
             image.style.zIndex = isBaseImage ? "1" : "2";
 
             body.appendChild(image);
-
+            // Armoire
             if (src === "/Images/Chambre/Armoire.svg") {
                 image.style.cursor = "pointer";
                 image.addEventListener('click', () => {
@@ -72,22 +72,25 @@ function openImageInNewTab() {
 
 
                     const objects = [
-                        { src: "/Images/Chambre/kholArmoire.png", descriptionSrc: "/Images/Chambre/khol.png", width: "12%", top: "45%", left: "45%" },
-                        { src: "/Images/Chambre/kerdoun.png", descriptionSrc: "/Images/Chambre/kardoun.png", width: "9%", top: "87%", left: "32%" },
-                        { src: "/Images/Chambre/laajar.png", descriptionSrc: "/Images/Chambre/laajar.png", width: "14%", top: "89%", left: "42%" },
-                        { src: "/Images/Chambre/albums.png", descriptionSrc: "/Images/Chambre/legende.png", width: "15%", top: "74%", left: "61%" },
-                        { src: "/Images/Chambre/Recette.png", descriptionSrc: "/Images/Chambre/Recette.png", width: "9%", top: "83%", left: "56%" },
-                        { src: "/Images/Chambre/Tapis.png", descriptionSrc: "/Images/Chambre/Tapis.png", width: "10%", top: "41%", left: "60%" },
+                        { src: "/Images/Chambre/kholArmoire.png", descriptionSrc: "/Images/Chambre/khol.png", width: "6%", top: "42%", left: "48%" },
+                        { src: "/Images/Chambre/kerdoun.png", descriptionSrc: "/Images/Chambre/kardoun.png", width: "10%", top: "89%", left: "43%" },
+                        { src: "/Images/Chambre/laajar.png", descriptionSrc: "/Images/Chambre/haik-aajar.png", width: "14%", top: "53%", left: "29%" },
+                        { src: "/Images/Chambre/albums.png", descriptionSrc: "/Images/Chambre/legende.png", width: "15%", top: "76%", left: "61%" },
+                        { src: "/Images/Chambre/Recette.png", descriptionSrc: "/Images/Chambre/couscous.png", width: "10%", top: "7%", left: "30%" },
+                        { src: "/Images/Chambre/Bracelet.png", descriptionSrc: "/Images/Chambre/Bracelet.png", width: "10%", top: "12%", left: "62%" },
+                        { src: "/Images/Chambre/collier.png", descriptionSrc: "/Images/Chambre/collier.png", width: "16%", top: "23%", left: "59%"},
+                        { src: "/Images/Chambre/Khalkhal.png", descriptionSrc: "/Images/Chambre/Khalkhal.png", width: "9%", top: "85%", left: "30%", rotate: "z -30deg" },
                     ];
 
 
-                    objects.forEach(({ src, descriptionSrc, width, top, left}) => {
+                    objects.forEach(({ src, descriptionSrc, width, top, left, rotate}) => {
                         const objectImage = new Image();
                         objectImage.src = src;
                         objectImage.style.width = width;
                         objectImage.style.position = "absolute";
                         objectImage.style.top = top;
                         objectImage.style.left = left;
+                        objectImage.style.rotate = rotate;
                         objectImage.style.cursor = "pointer";
                         objectImage.style.zIndex = "110";
 
@@ -156,7 +159,7 @@ function openImageInNewTab() {
                     newTab.document.body.appendChild(popup);
                 });
             }
-
+            //Coffre
             if (src === "/Images/Chambre/Coffre.svg") {
                 image.style.cursor = "pointer";
                 image.addEventListener('click', () => {
@@ -199,7 +202,53 @@ function openImageInNewTab() {
 
                     newTab.document.body.appendChild(popup);
                 });
+            }   
+            //Cadre
+            if (src === "/Images/Chambre/CadreFamille.png") {
+                image.style.cursor = "pointer";
+                image.addEventListener('click', () => {
+                    const popup = newTab.document.createElement('div');
+                    popup.style.position = "fixed";
+                    popup.style.top = "0";
+                    popup.style.left = "0";
+                    popup.style.width = "100%";
+                    popup.style.height = "100%";
+                    popup.style.background = "rgba(0, 0, 0, 0.7)";
+                    popup.style.display = "flex";
+                    popup.style.justifyContent = "center";
+                    popup.style.alignItems = "center";
+                    popup.style.zIndex = "100";
+
+                    const fullImage = new Image();
+                    fullImage.src = "/Images/Chambre/famille.png"; 
+                    fullImage.style.width = "50%"; 
+                    fullImage.style.borderRadius = "10px";
+
+                    const closePopup = newTab.document.createElement('button');
+                    closePopup.textContent = "Fermer";
+                    closePopup.style.marginTop = "20px";
+                    closePopup.style.background = "#DEB887";
+                    closePopup.style.color = "white";
+                    closePopup.style.border = "none";
+                    closePopup.style.padding = "10px 20px";
+                    closePopup.style.cursor = "pointer";
+                    closePopup.style.borderRadius = "10px";
+                    closePopup.style.position = "absolute";
+                    closePopup.style.top = "0%";
+                    closePopup.style.left = "93.5%";
+
+                    closePopup.addEventListener('click', () => {
+                        popup.style.display = "none";
+                    });
+
+                    popup.appendChild(fullImage);
+                    popup.appendChild(closePopup);
+
+                    newTab.document.body.appendChild(popup);
+                });
             }
+
+
 
         });
     } else {
